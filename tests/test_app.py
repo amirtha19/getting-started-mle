c3e9c3ce6fbe997b04b8de24c76a864b3c852212
amirthavarshini-19 <amirtha.varshini@smallcase.com>
2026-02-28 17:10:47 +0530

    remove venv

--- tests/test_app.py
+++ tests/test_app.py
@@ -1,9 +1,8 @@
 from fastapi.testclient import TestClient
-from app import app
+from app.app import app
 
 client = TestClient(app)
 
 def test_home():
     response = client.get("/")
     assert response.status_code == 200
-    
